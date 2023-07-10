95d64d89b056bc3a856ee9d9b4b18871d45d4404
Akvilė <121254516+Zebraszebras@users.noreply.github.com>
2023-07-10 12:55:52 +0300

    Header style completed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import userModel from "@/models/userModel";
 import { useState, useEffect } from "react";
+import styles from "./styles.module.css";
+import logo from "../../assets/question.jpg";
 
 const Header = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -17,14 +19,18 @@ const Header = () => {
 
     return (
         <>
-            <div>
+            <div className={styles.header}>
+                <img className={styles.logo}src={logo.src} alt="Question logo"></img>
                 Header
-                <Link href="/">Main</Link>
-                {isLoggedIn && <Link href="/ask">Ask Question</Link>}
-                {!isLoggedIn && <Link href="/login">Login</Link>}
-                {!isLoggedIn && <Link href="/signup">Signup</Link>}
-                {userName && <label>Welcome, {userName} </label>}
-                {isLoggedIn && <button onClick={signOut}>Sign out</button> }
+                <ul>
+                    <li>
+                <Link href="/">Main</Link> </li>
+               <li> {isLoggedIn && <Link href="/ask">Ask Question</Link>} </li>
+               <li> {!isLoggedIn && <Link href="/login">Login</Link>} </li>
+                <li>{!isLoggedIn && <Link href="/signup">Signup</Link>}</li>
+               <li className={styles.welcome}> {userName && <label>Welcome, {userName} </label>} </li>
+               <li > {isLoggedIn && <button className={styles.signOut} onClick={signOut}>Sign out</button> }</li>
+                </ul>
             </div>
         </>
     )
